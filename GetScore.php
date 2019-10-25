30d0c0521eeea8a8368024dcbc10f44aaadbede9
Thomas De Queiros <32330439+thomasdqr@users.noreply.github.com>
2019-10-25 10:38:17 +0200

    Add files via upload

---
+++ GetScore.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$city = $_GET['city'];
+$score = $_GET['score'];
+
+$dbh = new PDO('mysql:host=thomasdeltroot.mysql.db;dbname=thomasdeltroot', 'thomasdeltroot', 'Password98');
+		
+$dbh->query("INSERT INTO eco (id, city, score) VALUES ('', '$city', '$score')");
+
+echo $city." ".$score;
+
+
+
+
+?>
---
+++ UpdateScore.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+$city = $_GET['city'];
+$score = $_GET['score'];
+
+$dbh = new PDO('mysql:host=thomasdeltroot.mysql.db;dbname=thomasdeltroot', 'thomasdeltroot', 'Password98');
+		
+$dbh->query("UPDATE eco SET score='$score' WHERE city='$city' ");
+
+$result = "";
+foreach($dbh->query("SELECT city, score FROM eco ORDER BY score DESC LIMIT 10") as $row) {
+	echo $row[0].";".$row[1].";";
+}
+
+
+?>
